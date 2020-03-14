0e65f6d4b009a1a297aeaddb587509cf6bd8138b
Samuel Čavoj <sammko@sammserver.com>
2020-03-14 19:48:44 +0100

    Update to v2

--- hid-glorious.c
+++ hid-glorious.c
@@ -1,9 +1,12 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 /*
- *  USB HID quirks support for Glorious PC Gaming Race
- *  Glorious Model O and O-.
+ *  USB HID driver for Glorious PC Gaming Race
+ *  Glorious Model O, O- and D mice.
  *
  *  Copyright (c) 2020 Samuel Čavoj <sammko@sammserver.com>
+ */
+
+/*
  */
 
 #include <linux/hid.h>
@@ -16,36 +19,57 @@ MODULE_DESCRIPTION("HID driver for Glorious PC Gaming Race mice");
 
 /*
  * Glorious Model O and O- specify the const flag in the consumer input
- * report descriptor, which leads to inputs being ignored.
+ * report descriptor, which leads to inputs being ignored. Fix this
+ * by patching the descriptor.
  */
 static __u8 *glorious_report_fixup(struct hid_device *hdev, __u8 *rdesc,
 		unsigned int *rsize)
 {
-    if (*rsize == 213 && rdesc[84] == 129 && rdesc[112] == 129 && rdesc[140] == 129 &&
-            rdesc[85] == 3 && rdesc[113] == 3 && rdesc[141] == 3) {
-        hid_info(hdev, "patching Glorious Model O consumer control report descriptor\n");
-        rdesc[85] = rdesc[113] = rdesc[141] = 6;
-    }
+	if (*rsize == 213 &&
+		rdesc[84] == 129 && rdesc[112] == 129 && rdesc[140] == 129 &&
+		rdesc[85] == 3   && rdesc[113] == 3   && rdesc[141] == 3) {
+		hid_info(hdev, "patching Glorious Model O consumer control report descriptor\n");
+		rdesc[85] = rdesc[113] = rdesc[141] = \
+			HID_MAIN_ITEM_VARIABLE | HID_MAIN_ITEM_RELATIVE;
+	}
 	return rdesc;
 }
 
-static int glorious_probe(struct hid_device *hdev, const struct hid_device_id *id)
+static void glorious_update_name(struct hid_device *hdev)
 {
-    int ret;
+	const char *model = "Device";
 
-    hdev->quirks |= HID_QUIRK_INPUT_PER_APP;
+	switch (hdev->product) {
+	case USB_DEVICE_ID_GLORIOUS_MODEL_O:
+		model = "Model O"; break;
+	case USB_DEVICE_ID_GLORIOUS_MODEL_D:
+		model = "Model D"; break;
+	}
 
-    ret = hid_parse(hdev);
-    if (ret)
-        return ret;
+	snprintf(hdev->name, sizeof(hdev->name), "%s %s", "Glorious", model);
+}
 
-    snprintf(hdev->name, sizeof(hdev->name), "%s", "Glorious Model O");
+static int glorious_probe(struct hid_device *hdev,
+		const struct hid_device_id *id)
+{
+	int ret;
 
-    return hid_hw_start(hdev, HID_CONNECT_DEFAULT);
+	hdev->quirks |= HID_QUIRK_INPUT_PER_APP;
+
+	ret = hid_parse(hdev);
+	if (ret)
+		return ret;
+
+	glorious_update_name(hdev);
+
+	return hid_hw_start(hdev, HID_CONNECT_DEFAULT);
 }
 
 static const struct hid_device_id glorious_devices[] = {
-	{ HID_USB_DEVICE(USB_VENDOR_ID_GLORIOUS, USB_DEVICE_ID_GLORIOUS_MODEL_O) },
+	{ HID_USB_DEVICE(USB_VENDOR_ID_GLORIOUS,
+		USB_DEVICE_ID_GLORIOUS_MODEL_O) },
+	{ HID_USB_DEVICE(USB_VENDOR_ID_GLORIOUS,
+		USB_DEVICE_ID_GLORIOUS_MODEL_D) },
 	{ }
 };
 MODULE_DEVICE_TABLE(hid, glorious_devices);
@@ -53,7 +77,7 @@ MODULE_DEVICE_TABLE(hid, glorious_devices);
 static struct hid_driver glorious_driver = {
 	.name = "glorious",
 	.id_table = glorious_devices,
-    .probe = glorious_probe,
+	.probe = glorious_probe,
 	.report_fixup = glorious_report_fixup
 };
 
